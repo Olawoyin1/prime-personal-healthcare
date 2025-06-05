64e8f5a4ec71fa1d7041b8f6a665b52b110e14a6
Olawoyin <yustee2017@gmail.com>
2025-06-05 22:57:09 +0100

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+     <div className=" text-center bg-blue-100 rounded-xl shadow">
+      <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind + React!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
